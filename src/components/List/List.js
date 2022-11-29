310f438c4d39429fc2e7889fd5b7167ec00551ac
Allan-SS <allansousasilva1@gmail.com>
2022-11-28 22:00:53 -0300

    feat: Add a pokedex Page and a function of add pokemons in pokedex

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Cards from "../Card/Cards";
 
-const List = ({ filterPokemons }) => {
+const List = ({ filterPokemons, add }) => {
   return (
     <Container>
       <Row>
-        {filterPokemons.map((pokemon) => (
-          <Col>
-            <Cards pokemon={pokemon} />
+        {filterPokemons.map((pokemon, index) => (
+          <Col key={index}>
+            <Cards pokemon={pokemon} add={add}/>
           </Col>
         ))}
       </Row>
---
+++ src/components/PokeDexBtn/ButtonDex.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import IconDex from "../../assets/smartphone.png";
+import "./ButtonDex.scss";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+const ButtonDex = ({ path, message }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(path);
+  };
+
+  const renderTooltip = (props) => (
+    <Tooltip className="AlertButton" id="button-tooltip" {...props}>
+      {message}
+    </Tooltip>
+  );
+
+  return (
+    <div className="ContainerButton">
+      <OverlayTrigger
+        placement="top"
+        delay={{ show: 100, hide: 250 }}
+        overlay={renderTooltip}
+      >
+        <button onClick={() => handleClick()}>
+          <img src={IconDex} alt="IconButton" />
+        </button>
+      </OverlayTrigger>
+    </div>
+  );
+};
+
+export default ButtonDex;
